ac668639e3d087085c6b4015870eb2206bba4519
Arun Gautham Soundarrajan <arungautham60@gmail.com>
2025-07-27 10:26:20 +0100

    removed json saving format

--- internal/io/io.go
+++ internal/io/io.go
@@ -1,7 +1,6 @@
 package io
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/url"
 	"os"
@@ -22,24 +21,6 @@ type PageMetadata struct {
 type PageData struct {
 	Metadata PageMetadata `json:"metadata"`
 	Content  string       `json:"content"`
-}
-
-func SavePageDataAsJSON(pageData PageData, filename string) error {
-	err := os.MkdirAll("output", 0755)
-	if err != nil {
-		return err
-	}
-
-	file, err := os.Create("output/" + filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-
-	return encoder.Encode(pageData)
 }
 
 func SavePageDataAsMarkdown(pageData PageData, filename string) error {
